c319c874978476049d56f9cecebb2fe2f6be3c1b
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-02 14:18:52 +0530

    feat: onboarding devcontainers-ci actions

---
+++ github-action/src/docker.ts
@@ -0,0 +1,79 @@
+import * as core from '@actions/core';
+import * as docker from '../../common/src/docker';
+import {exec} from './exec';
+
+export async function isDockerBuildXInstalled(): Promise<boolean> {
+	return await docker.isDockerBuildXInstalled(exec);
+}
+export async function buildImage(
+	imageName: string,
+	imageTag: string | undefined,
+	checkoutPath: string,
+	subFolder: string,
+	skipContainerUserIdUpdate: boolean,
+	cacheFrom: string[],
+	cacheTo: string[],
+): Promise<string> {
+	core.startGroup('🏗 Building dev container...');
+	try {
+		return await docker.buildImage(
+			exec,
+			imageName,
+			imageTag,
+			checkoutPath,
+			subFolder,
+			skipContainerUserIdUpdate,
+			cacheFrom,
+			cacheTo,
+		);
+	} catch (error) {
+		core.setFailed(error);
+		return '';
+	} finally {
+		core.endGroup();
+	}
+}
+
+export async function runContainer(
+	imageName: string,
+	imageTag: string | undefined,
+	checkoutPath: string,
+	subFolder: string,
+	command: string,
+	envs?: string[],
+): Promise<boolean> {
+	core.startGroup('🏃‍♀️ Running dev container...');
+	try {
+		await docker.runContainer(
+			exec,
+			imageName,
+			imageTag,
+			checkoutPath,
+			subFolder,
+			command,
+			envs,
+		);
+		return true;
+	} catch (error) {
+		core.setFailed(error);
+		return false;
+	} finally {
+		core.endGroup();
+	}
+}
+
+export async function pushImage(
+	imageName: string,
+	imageTag: string | undefined,
+): Promise<boolean> {
+	core.startGroup('📌 Pushing image...');
+	try {
+		await docker.pushImage(exec, imageName, imageTag);
+		return true;
+	} catch (error) {
+		core.setFailed(error);
+		return false;
+	} finally {
+		core.endGroup();
+	}
+}
