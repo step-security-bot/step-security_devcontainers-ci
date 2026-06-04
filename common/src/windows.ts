1df3136b89ebe392fe7ede1f7dc077313270d3cd
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-04 11:40:08 +0530

    comments addressed

--- common/src/windows.ts
+++ common/src/windows.ts
@@ -1,5 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright (c) 2026 StepSecurity
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
