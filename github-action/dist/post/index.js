aa5db93107ef5021c9d6164ce71ff29c0b42a3f6
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-06-04 11:49:53 +0530

    dist updated

--- github-action/dist/post/index.js
+++ github-action/dist/post/index.js
@@ -34204,6 +34204,7 @@ function parseGroup(input) {
 
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright (c) 2026 StepSecurity
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
